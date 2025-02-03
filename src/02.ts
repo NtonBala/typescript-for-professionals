b1513074f7547068c28145a20a0480f974a88bb5
Anton Balashov <balashov.anton.1983@gmail.com>
2025-02-03 14:50:09 +0200

    Add Queue class (FIFO collection)

--- src/02.ts
+++ src/02.ts
@@ -3,3 +3,17 @@
 const regexp: RegExp = new RegExp('ab+c');
 const array: Array<number> = [1, 2, 3];
 const set: Set<number> = new Set([1, 2, 3]);
+
+// * A first in first out collection
+class Queue<T> {
+  private data: Array<T> = [];
+
+  push(item: T) {
+    this.data.push(item);
+  }
+  pop(): T | undefined {
+    return this.data.shift();
+  }
+}
+
+let queue: Queue<number> = new Queue();
