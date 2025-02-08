9ae55fe979416cbce594bc1beec22e438d5a5a58
Anton Balashov <balashov.anton.1983@gmail.com>
2025-02-08 14:40:42 +0200

    Solve issue via generics

--- src/11.ts
+++ src/11.ts
@@ -1,29 +1,20 @@
 // Generics
 
 // A FIFO collection:
-class Queue {
-  data: any[] = [];
+class Queue<T> {
+  data: T[] = [];
 
-  push(item: any) {
+  push(item: T) {
     this.data.push(item);
   }
-  pop(): any {
+  pop(): T | undefined {
     return this.data.shift();
   }
 }
 
-class NumberQueue extends Queue {
-  push(item: number) {
-    super.push(item);
-  }
-  pop(): number {
-    return super.pop();
-  }
-}
-
-const queue = new NumberQueue();
+const queue = new Queue<number>();
 queue.push(123);
 queue.push(456);
 
-console.log(queue.pop().toPrecision(1));
-console.log(queue.pop().toPrecision(1));
+console.log(queue.pop()?.toPrecision(1));
+console.log(queue.pop()?.toPrecision(1));
