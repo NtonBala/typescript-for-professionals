00a21b07cd4543b67f38a4c21550b236be8da76a
Anton Balashov <balashov.anton.1983@gmail.com>
2025-02-11 17:36:35 +0200

    Add unknown usage example

--- src/12.ts
+++ src/12.ts
@@ -9,3 +9,14 @@ exampleAny = 'Hello';
 
 exampleAny.allows.anything.you.can.imaging(); // Each member is of type any
 let anySetBool: boolean = exampleAny; // any can be assigned to any type
+
+// unknown
+exampleUnknown = 123;
+exampleUnknown = 'World';
+
+if (typeof exampleUnknown === 'string') {
+  exampleUnknown.trim();
+}
+if (typeof exampleUnknown === 'boolean') {
+  let unknownSetBool: boolean = exampleUnknown;
+}
