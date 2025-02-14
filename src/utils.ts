e3924d57f7ef09e478a47944f5d4e82caa7d1ac5
Anton Balashov <balashov.anton.1983@gmail.com>
2025-02-14 15:45:43 +0200

    Add isPalindrome utility

---
+++ src/utils.ts
@@ -0,0 +1,9 @@
+/**
+ * @returns true if the input string is a palindrome
+ */
+function isPalindrome(str: string): boolean {
+  return str === str.split('').reverse().join('');
+}
+
+console.log(isPalindrome('madam')); // true
+console.log(isPalindrome('banana')); // false
